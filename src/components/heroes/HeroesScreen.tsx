e35bb935d3bd3c471e4e1e444e90798112258865
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-02-14 03:46:13 -0500

    docs

--- src/components/heroes/HeroesScreen.tsx
+++ src/components/heroes/HeroesScreen.tsx
@@ -6,7 +6,6 @@ import {
   useNavigate,
 } from "react-router-dom";
 import { getHeroesById } from "../../selectors/getHeroes";
-import MarvelScreen from "../marvel/MarvelScreen";
 import { useMemo } from "react";
 
 const HeroesScreen = () => {
