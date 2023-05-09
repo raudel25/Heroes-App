7c6ca30b5af837b06e63497902167f33bdcaf3c7
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-05-08 22:12:16 -0400

    fix

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,17 +1,17 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginScreen from "../components/login/LoginScreen";
 import DashboardRouters from "./DashboardRouters";
 
 const AppRouter = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div>
         <Routes>
           <Route path="/login" element={<LoginScreen />}></Route>
           <Route path="*" element={<DashboardRouters />}></Route>
         </Routes>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
